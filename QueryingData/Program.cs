7e0c6943ad7701a8f12a64e2299a64ea8913d23c
Houssnou <hosni.moubarak@gmail.com>
2025-06-19 16:05:57 -0400

    added Spectre console for better ui display

---
+++ QueryingData/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using QueryingData.Options;
+using QueryingData.QueryingData;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddOptions<ConfigOptions>()
+    .Bind(config: builder.Configuration.GetSection(key: ConfigOptions.SectionName))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+// Register SingleValueQueryManager as a service
+builder.Services.AddTransient<SingleValueQueryManager>();
+
+var app = builder.Build();
+
+// Get an instance from the service provider
+var singleValueQueryManager = app.Services.GetRequiredService<SingleValueQueryManager>();
+
+// Call the method and get the count
+var count = singleValueQueryManager.GetTotalMoviesCountAsync().GetAwaiter().GetResult();
+
+Console.WriteLine($"Total movies count: {count}");
+
+Console.WriteLine("Dapper demo app. Press Enter to exit.");
+Console.ReadLine();
---
+++ QueryingData/QueryingData/SingleValueQueryManager.cs
@@ -0,0 +1,31 @@
+﻿
+using Dapper;
+using Microsoft.Extensions.Options;
+using Npgsql;
+using QueryingData.Options;
+
+namespace QueryingData.QueryingData
+{
+    internal class SingleValueQueryManager
+    {
+        private readonly IOptions<ConfigOptions> _configOptions;
+
+        public SingleValueQueryManager(IOptions<ConfigOptions> configOptions)
+        {
+            _configOptions = configOptions;
+        }
+
+        /// <summary>
+        /// Retrieves the total number of movies in the database.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> GetTotalMoviesCountAsync()
+        {
+            using var connection = new NpgsqlConnection(_configOptions.Value.DefaultConnection);
+
+            var sql = "SELECT COUNT(*) FROM Film";
+            var count = await connection.ExecuteScalarAsync<int>(sql);
+            return count;
+        }
+    }
+}
